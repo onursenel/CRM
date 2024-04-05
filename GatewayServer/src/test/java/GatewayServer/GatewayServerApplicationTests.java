5a77e65148242247eecaff77acf0470ed1ba3d20
Eren <erengulmz@gmail.com>
2024-04-05 10:57:48 +0300

    .

---
+++ GatewayServer/src/test/java/GatewayServer/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package GatewayServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
