6434232a61af1104ce91bb9dba518830a1db9e2a
Onurcan Şenel <onrsenel@gmail.com>
2024-05-11 16:17:33 +0300

    .

--- customerservice/src/main/java/com/etiya/customerservice/entities/Customer.java
+++ customerservice/src/main/java/com/etiya/customerservice/entities/Customer.java
@@ -28,6 +28,9 @@ public class Customer extends BaseEntity {
     @OneToOne(mappedBy = "customer")
     private ContactMedium contactMedium;
 
+    @Column(name = "email")
+    private String email;
+
     @OneToMany(mappedBy = "customer")
     private List<Address> addresses;
 
